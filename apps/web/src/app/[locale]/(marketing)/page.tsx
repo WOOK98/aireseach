870044b4c6be36039eee897b1a459ca1dbd5f808
WOOK98 <zheng784486738@gmail.com>
2026-05-28 13:54:38 +0800

    remove Features & Testimonials from homepage, add overhaul checklist

--- apps/web/src/app/[locale]/(marketing)/page.tsx
+++ apps/web/src/app/[locale]/(marketing)/page.tsx
@@ -6,16 +6,12 @@ import { Pricing } from "~/modules/billing/pricing/pricing";
 import { PricingSectionSkeleton } from "~/modules/billing/pricing/section";
 import { Banner } from "~/modules/marketing/home/banner";
 import { Faq } from "~/modules/marketing/home/faq";
-import { Features } from "~/modules/marketing/home/features";
 import { Hero } from "~/modules/marketing/home/hero";
-import { Testimonials } from "~/modules/marketing/home/testimonials";
 
 const HomePage = () => {
   return (
     <>
       <Hero />
-      <Features />
-      <Testimonials />
       <Suspense fallback={<PricingSectionSkeleton />}>
         <Pricing />
       </Suspense>
